56829d6fb2a009973b36ca5dfcde36bbb7ce83e8
zheshishenme <thomas.fauquignon@orange.fr>
2014-02-13 15:32:55 +0100

    Create choixEvenement.php

---
+++ choixEvenement.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="choixevenement.css"/>
+        <title> Les évènements</title>
+    </head>
+    <body>
+        <header>
+            
+            <img src="images/logoweb.PNG" alt="logo" width= "200" height= "100">
+            <img src="images/pour votre hygièene de vie.PNG" alt="logo" width= "200" height= "50">
+            <img src="images/sportevents.PNG" alt="logo" width= "300" height= "100">
+            <img src="images/suzy.jpg" alt="logo" class="identifiant" width= "50" height= "50">
+            <div id="identifiant">
+                <?php
+		require 'connect.php';
+		
+		
+		$requete= "select prenom from IDENTIFIANT";
+                $resultat=mysql_query($requete);
+                echo ("bienvenue $resultat");
+                ?>
+            </div>
+           
+        </header>
+        <nav>
+            
+               et bien voilà 
+           
+        </nav>
+        
+        
+        <section>
+          
+            <div id="evenement">
+                
+                <img src="images/piscine.PNG" alt="logo" width= "200" height= "100">
+            </div>
+            
+        </section>
+        
+        <footer>
+           
+               sportevents, tous droits réservés
+            
+        </footer>
+        
+    </body>
+</html>
