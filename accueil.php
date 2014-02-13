0599e644a5280152707106d2d870bb3081823dcb
zheshishenme <thomas.fauquignon@orange.fr>
2014-02-13 15:30:42 +0100

    Update and rename accueil.phase to accueil.php

---
+++ accueil.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="accueil.css"/>
+        <title> sportevents_Accueil</title>
+    </head>
+    <body>
+    <center>
+        <img src="images/logo.PNG" alt=logo" width= "600" height= "500"> <br/>
+        
+        <a href="page accueil.php" class="bouton">entrer sur le site</a>
+        </center>
+    <?php
+		require 'connect.php';
+		?>
+    
+    </body>
+</html>
